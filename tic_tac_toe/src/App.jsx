f3b68555f7b3e9b1bcec1499d50970646b5014e7
Roshan Arikar <roshan.g.arikar@gmail.com>
2022-05-20 14:21:54 +0530

    create tic tac toe

---
+++ tic_tac_toe/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h2 style={color=green}>Tic Tac Toe</h2>
+    </div>
+  )
+}
+
+export default App
